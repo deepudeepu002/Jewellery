48b96ca0597a349e8eb5bd53cc361953d4d94e5b
sajinrajan <sajinrajan082@gmail.com>
2024-10-02 02:04:08 +0530

    blog commit

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// var btnOne = document.getElementById('read-more-one');
+// var btnTwo = document.getElementById('read-more-two');
+// var btnThree = document.getElementById('read-more-three');
+// var btnFour = document.getElementById('read-more-four');
+
+// function buttonOne(){
+//     btnOne 
+// }
+
+
 document.querySelectorAll('.read-more-btn').forEach(button => {
     button.addEventListener('click', () => {
         const postContent = button.previousElementSibling;
